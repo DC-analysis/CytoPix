2c3341efaf733deb5805823bb539139345e11dfc
Paul Müller <dev@craban.de>
2025-03-25 08:49:21 +0100

    enh: pretty-print logs

--- cytopix/_main.py
+++ cytopix/_main.py
@@ -1,8 +1,33 @@
+import logging
+
+
 class DevNull:
     """Effectively a file-like object for piping everything to nothing."""
 
     def write(self, *args, **kwargs):
         pass
+
+
+class PrettyFormatter(logging.Formatter):
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(levelname)s %(name)s: %(message)s"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
 
 
 def main():
@@ -22,6 +47,14 @@ def main():
         sys.stdout = DevNull()
     if sys.stderr is None:
         sys.stderr = DevNull()
+
+    # Tell the root logger to pretty-print logs
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(PrettyFormatter())
+    root_logger.addHandler(handler)
 
     from .main import CytoPix
 
